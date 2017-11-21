ee7a7d505733f9a4a17d0d5f96dbd9de42fd74f1
undergraduate <cse20141549@cspro1.sogang.ac.kr>
2017-11-21 18:56:26 +0900

    p6 scatter coded.

--- proj1/p6/par_image_proc.c
+++ proj1/p6/par_image_proc.c
@@ -1,3 +1,4 @@
+#include "ppm.h"
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -46,9 +47,9 @@ void derived_RGB_Type()
 
 
 }
-void derivedPixelType()
+void derived_Pixel_Type()
 {
-	Pixel sample;
+	PIXEL sample;
 
 	int block_len[2];
 	
@@ -59,22 +60,69 @@ void derivedPixelType()
 	block_len[0] = block_len[1] = 1;
 
 	types[0] = MPI_INT;
-	types[1] = RGB_Type;
+	types[1] = RGB_type;
 
 	displ[0] = 0;
 	MPI_Get_address(&sample.offset, &origin);
 	MPI_Get_address(&sample.rgb, &off);
 	displ[1] = off - origin;
 
-	MPI_Type_create_struct(2, blklen, displ, types, &Pixel_Type);
+	MPI_Type_create_struct(2, block_len, displ, types, &Pixel_type);
 	MPI_Type_commit(&Pixel_type);
+}
+void scatter_img_block(RGB *sendbuf, int send_block_cnt,
+					   int **p_sendcounts, int **p_displs,
+					   RGB **p_recvbuf, int *p_recv_block_cnt, int block_size)
+{
+	int i;
+	int idx = 0;
+	if (rank == root) 
+	{
+		*p_sendcounts = malloc(sizeof(int) * size);
+		*p_displs = malloc(sizeof(int) * size);
+
+
+		for(i = 0; i < size; ++i) 
+		{
+			(*p_displs)[i] = idx;
+
+			if (i < send_block_cnt % size)
+				(*p_sendcounts)[i] = send_block_cnt / size + 1;
+			else
+				(*p_sendcounts)[i] = send_block_cnt / size;
+
+			idx += (*p_sendcounts)[i];
+		}
+	}
+
+	if (rank < send_block_cnt % size)
+		*p_recv_block_cnt = send_block_cnt / size + 1;
+	else
+		*p_recv_block_cnt = send_block_cnt / size;
+
+	*p_recvbuf = malloc(sizeof(RGB) * block_size * (*p_recv_block_cnt));
+	MPI_Scatterv(sendbuf, 
+			*p_sendcounts, 
+			*p_displs, 
+			RGB_RowGroup_type,	 
+			*p_recvbuf, 
+			*p_recv_block_cnt, 
+			RGB_RowGroup_type, 
+			root, MPI_COMM_WORLD);
 }
 
 
 
 int main(int argc, char *argv[])
 {
+	int width,height;
 	struct timeval Tstart, Tend; //time value
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	PPMImage img;
 
 	if( argc < 3)
 	{
@@ -85,6 +133,29 @@ int main(int argc, char *argv[])
 	gettimeofday(&Tstart, NULL); 
 
 	//implement
+	if (rank == root) {
+		fnReadPPM(argv[1], &img);
+		width = img.width;
+		height = img.height;
+	}
+
+	MPI_Bcast(&width, 1, MPI_INT, root, MPI_COMM_WORLD);
+	MPI_Bcast(&height, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+	derived_RGB_Type();
+	derived_Pixel_Type();
+	MPI_Type_vector(1, width, 1, RGB_type, &RGB_RowGroup_type);
+	MPI_Type_commit(&RGB_RowGroup_type);
+
+	RGB *data = NULL;
+	int data_block_cnt;
+	int *counts = NULL, *displs = NULL;
+	scatter_img_block(img.pixels, height,
+					  &counts, &displs,
+					  &data, &data_block_cnt, width);
+
+	printf("[%d] received data block cnt = %d\n", rank, data_block_cnt);
+
 
 	
 	
